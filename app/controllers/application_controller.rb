5593017f6021f99986af79e0c32957573f6e5427
qternel <sidorchenko@sfedu.ru>
2025-05-07 23:39:14 +0300

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+  def decode_token(token)
+    HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
+  rescue
+    nil
+  end
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header&.split(" ")&.last
+    decoded = decode_token(token)
+    if decoded
+      @current_user = User.find(decoded[:user_id])
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :unauthorized
+  end
 end
---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,30 @@
+class AuthController < ApplicationController
+  skip_before_action :authorize_request, only: [:signup, :login]
+
+  def signup
+    user = User.new(user_params)
+    if user.save
+      token = encode_token(user_id: user.id)
+      render json: { token: token, user_id: user.id }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: 422
+    end
+  end
+
+  def login
+    user = User.find_by(login: params[:login])
+    if user&.authenticate(params[:password])
+      token = encode_token(user_id: user.id)
+      render json: { token: token }
+    else
+      render json: { error: "Invalid credentials" }, status: :unauthorized
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.permit(:login, :password, :password_confirmation)
+  end
+end
