55ef5e967fbad6cf1365c342fc831a16f39a4be3
4th-KiNG <avpisanko@mail.ru>
2025-05-28 14:45:35 +0300

    1

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const api = axios.create();
 
 const token = localStorage.getItem("token");
-if (token) api.defaults.headers["Autorization"] = token;
+if (token) api.defaults.headers["Autorization"] = `Bearer ${token}`;
 
 export default api;
