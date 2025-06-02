f46f63641480c69d6328efdac7e4129668183652
Ростислав Шведов <rustysquezzy@outlook.com>
2025-06-03 02:42:49 +0300

    deleted trash files

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Добро пожаловать в Cookie Jar 🍪</h1>
-      <p>Тут будет список твоих достижений!</p>
-    </div>
-  );
-}
